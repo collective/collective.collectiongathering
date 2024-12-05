fcd9778999b18ca0817ea78b3b4d7ff54f6547a9
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-12-05 12:59:41 +0100

    update template and view with Plone 6

--- src/collective/collectiongathering/browser/views.py
+++ src/collective/collectiongathering/browser/views.py
@@ -5,8 +5,18 @@ from collective.collectiongathering.interfaces import ICollectionGathering
 
 
 class CollectionGatheringView(CollectionView):
-    def __init__(self, *args, **kwargs):
-        super(CollectionView, self).__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     super(CollectionView, self).__init__(*args, **kwargs)
+    #     context = aq_inner(self.context)
+    #     # self.collection_behavior = ICollectionGathering(context)
+    #     # self.b_size = self.collection_behavior.item_count
+
+    @property
+    def collection_behavior(self):
         context = aq_inner(self.context)
-        self.collection_behavior = ICollectionGathering(context)
-        self.b_size = self.collection_behavior.item_count
+
+        return ICollectionGathering(context)
+
+    @property
+    def b_size(self):
+        return self.collection_behavior.item_count

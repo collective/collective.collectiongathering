a1b97eba96d383b0eb22af27ee375a185fb4d133
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-12-05 16:49:24 +0100

    pythonv ersions

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.11",
     install_requires=[
         "setuptools",
         "Acquisition",
